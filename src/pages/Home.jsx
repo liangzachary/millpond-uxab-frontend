7f2ca773f6c9c96a65bdd0fb03359cc424d9215c
Zachary Liang <liangzachary20@gmail.com>
2025-11-24 06:16:12 -0800

    Navbar and Footer issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 import HeroSection from "../components/home/HeroSection";
 import StrategyRisksSection from "../components/home/StrategyRisksSection";
